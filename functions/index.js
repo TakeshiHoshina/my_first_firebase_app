77aad20152d9ceb943b191a27683baa351b45623
Takeshi Hoshina <takeshi_hdz@me.com>
2021-02-16 23:29:34 +0900

    added open weather api access

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,31 @@
 const functions = require("firebase-functions");
 const express = require('express');
+const requestPromise = require('request-promise');
 
 const app = express();
+
+const getDataFromApi = async (cityname) => {
+  try {
+    const apiKey = functions.config().openwheather.key;
+    const url = (`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${functions.config().openwheather.key}`)
+    const result = await requestPromise({
+      uri: url,
+      method: 'GET'
+    });
+    return result;
+  } catch (error) {
+    return ('Error caught : ' + error.message);
+  }
+};
+
+app.get('/weather/:cityname', (req, res) => {
+  getDataFromApi(req.params.cityname).then((response) => res.send(response));
+});
+
+app.get('/apikey', (req, res) => {
+  const apiKey = functions.config().openwheather.key;
+  res.send('API Key is ' + apiKey);
+});
 
 app.get('/hello', (req, res) => {
   res.send('Hello Express!!');
